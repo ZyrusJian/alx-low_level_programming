8e501b1df4a2ceae949fb3aa2ded0e6df1da6450
Zelalem <zzewdie@gmail.com>
2022-09-09 05:20:26 +0300

    Cprograming_puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* this program uses the puts() function to print
+ * a string exactly followed by a newline. */
+#include<stdio.h>
+int main() {
+	char sentence[]="\"Programming is like building a multilingual puzzle\n";
+	puts(sentence);
+	return 0;
+}
