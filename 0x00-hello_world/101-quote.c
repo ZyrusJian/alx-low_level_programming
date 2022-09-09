42ad9fd0f3443809f0ba4ed6f88abb9bbafa6003
Zelalem <zzewdie@gmail.com>
2022-09-09 12:57:58 +0300

    Cprograming_stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+/*
+ * this program prints to the standard error
+ * a string exactly followed by a newline.
+*/
+#include<stdio.h>
+/**
+ * main - displays a string to the stderr
+ * Return: 1 for succes.
+ */
+int main(void)
+{
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int er = sizeof(err);
+
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	er, 1, stderr);
+
+return (1);
+}
