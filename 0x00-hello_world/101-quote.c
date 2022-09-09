1f35d02972c063d8ce32cea98336f79a673c847b
Zelalem <zzewdie@gmail.com>
2022-09-09 13:38:21 +0300

    Cprograming_stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,7 @@
  * a string exactly followed by a newline.
 */
 #include<stdio.h>
+#include<string.h>
 /**
  * main - displays a string to the stderr
  * Return: 1 for succes.
@@ -10,7 +11,7 @@
 int main(void)
 {
 char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-int er = sizeof(err);
+int er = strlen(err);
 
 fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 1,
 	er, stderr);
