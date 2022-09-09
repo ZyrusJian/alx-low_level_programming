77cc496229b0c1d8e03ab465297d8ea2e0312b95
Zelalem <zzewdie@gmail.com>
2022-09-09 13:01:16 +0300

    Cprograming_stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int er = sizeof(err);
 
 fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19",
