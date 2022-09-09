a1111f36a71a5f12aa267afeb162f8464906313c
Zelalem <zzewdie@gmail.com>
2022-09-10 01:25:02 +0300

    Cprograming_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * this code uses WHILE loop condition to print
+ * lower case a -z using putchar().
+*/
+#include <stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - printout the alphabet in lower case using putchar().
+ * Return: 0 for success.
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp++;
+	}
+	putchar('\n');
+	return (0);
+}
