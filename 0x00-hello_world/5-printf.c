0e14ed36198cb05f582ceb7c1f3307f177532662
Zelalem <zzewdie@gmail.com>
2022-09-09 11:44:04 +0300

    Cprograming_printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/*
+ * this program uses the printf() function to print
+ * a string exactly followed by a newline.
+*/
+#include<stdio.h>
+/**
+ * main - displays a string
+ * Return: o for succes.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
