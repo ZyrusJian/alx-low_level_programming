cd39216827076ad99e52d2c0c819a26c2807af30
Zelalem <zzewdie@gmail.com>
2022-09-10 04:36:40 +0300

    Cprograming_forif

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,8 +17,11 @@ int main(void)
 	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
