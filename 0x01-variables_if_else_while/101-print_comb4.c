88451290511f354a3aa09620487fb1a0d7079624
Zelalem <zzewdie@gmail.com>
2022-09-10 05:05:19 +0300

    task_101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+/*
+ * this code uses FOR loop condition to print
+ * possible combination of single digit
+ * numbers in ascending order using putchar().
+*/
+#include <stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - printout combination of 0-9 using putchar().
+ * Return: 0 for success.
+ */
+int main(void)
+{
+	int n;
+	int k;
+	int j;
+
+	for (n = 0; n < 8; n++)
+	{
+		for (k = 1; k < 9; k++)
+		{
+			for (j = 2; j < 10; j++)
+			{
+				if (j > k)
+				{
+					putchar(n + '0');
+					putchar(k + '0');
+					putchar(j + '0');
+					if (n != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
