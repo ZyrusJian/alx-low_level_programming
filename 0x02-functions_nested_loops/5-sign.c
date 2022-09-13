c140b255b8836c78cce3a32435d46ba2c1b762e0
Zelalem <zzewdie@gmail.com>
2022-09-13 23:10:07 +0300

    Cprograming_func_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - checks whether a number is positive or negative.
+ * @n: number to be checked.
+ *
+ * Return: 1 for positive. 0 for zero. -1 for negative.
+ */
+
+int print_sign(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-)';
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
