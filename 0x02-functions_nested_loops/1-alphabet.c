4b4273baa4551bd74f55363b82dde93c5ed749a0
Zelalem <zzewdie@gmail.com>
2022-09-13 21:56:42 +0300

    Cprograming_functions_2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lower case
+ *
+ *  
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
