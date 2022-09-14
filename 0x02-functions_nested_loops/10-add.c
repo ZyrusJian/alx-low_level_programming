75a53adedd895f80890f6c72dcdaf7f5d5412c36
Zelalem <zzewdie@gmail.com>
2022-09-15 00:02:22 +0300

    Cprograming_func_add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - computes the sum of given two integers.
+ *@a: target integer
+ *@b: target integer
+ *
+ * Return: result after computing.
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
