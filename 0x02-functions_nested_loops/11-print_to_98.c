6a217c4e5b12c7680d92de6a542f9b4f870adb3a
Zelalem <zzewdie@gmail.com>
2022-09-15 00:25:09 +0300

    Cprograming_func_counter

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * print_to_98 - print integer from n-98..
+ *@n: target integer
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf(i);
+			if (i < 98)
+				printf(", ");
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i <= 98; i--)
+		{
+			printf(i);
+			if (i > 98)
+				printf(", ");
+		}
+	}
+}
