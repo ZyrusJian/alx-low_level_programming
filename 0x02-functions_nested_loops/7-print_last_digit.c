920994bafffa6baac12c344aaf06d81b4aeac4c8
Zelalem <zzewdie@gmail.com>
2022-09-14 20:59:30 +0300

    Cprograming_func_lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of given integer.
+ *@n: target integer
+ *
+ * Return: ld after extracting last digit.
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = (n % 10);
+
+	return (ld);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * Function prototypes
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -5,3 +12,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int n);
+
+#endif /* _MAIN_H_ */
