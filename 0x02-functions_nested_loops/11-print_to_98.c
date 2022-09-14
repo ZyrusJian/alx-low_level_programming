106901f825bd190adc8f74707d162c3ca78ae6d0
Zelalem <zzewdie@gmail.com>
2022-09-15 00:50:31 +0300

    Cprograming_func_counter

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -26,7 +26,9 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = n; i <= 98; i--)
+		i = n;
+
+		while (i >= 98 )
 		{
 			printf("%d", i);
 			if (i > 98)
@@ -34,6 +36,7 @@ void print_to_98(int n)
 				putchar(',');
 				putchar(' ');
 			}
+			i--;
 		}
 		printf("\n");
 	}
