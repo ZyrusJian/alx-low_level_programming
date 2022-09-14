e5e1be81ac96b7da196dcf40b2dfdf535d37924f
Zelalem <zzewdie@gmail.com>
2022-09-14 22:00:26 +0300

    Cprograming_func_timer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * jack_bauer - printout every minute in a 24 hour.
+ *
+ */
+
+void jack_bauer(void)
+{
+	int n = 0;
+	int k;
+	int j;
+	int i;
+
+	while (n < 3)
+	{
+		k = 0;
+
+		while (k < 4)
+		{
+			j = 0;
+
+			while (j < 6)
+			{
+				i = 0;
+
+				while (i < 10)
+				{
+					_putchar(n + '0');
+					_putchar(k + '0');
+					_putchar(':');
+					_putchar(j + '0');
+					_putchar(i + '0');
+					_putchar('\n');
+					i++;
+				}
+
+				j++;
+			}
+
+			k++;
+		}
+
+		n++;
+	}
+
+}
