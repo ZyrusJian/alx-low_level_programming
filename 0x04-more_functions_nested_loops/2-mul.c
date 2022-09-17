999c9bb3a0b175c760560b031cbffb3441a37295
Zelalem <zzewdie@gmail.com>
2022-09-17 03:46:37 +0300

    Cprograming_morefunc_2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul - multiplies two given integers..
+ * @a: integer target
+ * @b: integer target.
+ *
+ * Return: product after computing..
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
