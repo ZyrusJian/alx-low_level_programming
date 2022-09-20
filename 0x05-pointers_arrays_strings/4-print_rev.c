280606dde7f80c3e21f6cae375a0971fe13832e5
Zelalem <zzewdie@gmail.com>
2022-09-20 22:31:03 +0300

    Cprog_pointers_4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - writes the string s to stdout in reverse
+ * @s: The string to print
+ *
+ */
+
+void print_rev(char *s);
+{
+	int i;
+
+	i = _strlen(*s);
+
+	while (i > 0)
+	{
+		_putchar(*(s + (i - 1)));
+		i--;
+	}
+	_putchar('\n');
+}
