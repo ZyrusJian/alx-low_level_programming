9e5167f9de7362ab82c95fb780b73f8a56734968
Zelalem <zzewdie@gmail.com>
2022-09-20 21:55:00 +0300

    Cprog_pointers_2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - computes the length of given string.
+ * @s: string to be evaluated.
+ *
+ * Return: len of string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		*s++;
+	}
+	return (len);
+}
