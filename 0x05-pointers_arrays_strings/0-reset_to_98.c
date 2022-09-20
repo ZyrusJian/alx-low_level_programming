f1207c1313b9d2129fa1d4db2e904a85af4c6e43
Zelalem <zzewdie@gmail.com>
2022-09-20 20:32:56 +0300

    Cprog_pointers_0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value of variable to 98 always..
+ * @n: variable to be evaluated.
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This folder concerns Cprograming projects on
+Pointers, Arrays and strings.
