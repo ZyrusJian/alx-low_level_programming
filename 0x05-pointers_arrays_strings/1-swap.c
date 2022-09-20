0ace4af69f59884a4743871321bb2923bd49fec9
Zelalem <zzewdie@gmail.com>
2022-09-20 20:59:22 +0300

    Cprog_pointers_1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of variable a to b and vice-versa.
+ * @a: variable to be evaluated.
+ * @b: variable to be evaluated.
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+	x = a;
+
+	*a = b;
+	*b = x;
+}
