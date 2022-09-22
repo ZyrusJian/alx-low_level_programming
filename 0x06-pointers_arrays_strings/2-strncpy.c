dbbae839b1af99f93bd63e571865e0d5a7e2eb5c
Zelalem <zzewdie@gmail.com>
2022-09-22 22:25:51 +0300

    Cprog_String_2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+int _strlen(char *str);
+
+/**
+ * _strncpy - copies n-bytes of the string src to dest
+ * @dest: The string target
+ * @src: the string to be copied
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest after appending.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int l1, l2, lt, j;
+
+	l1 = _strlen(dest);
+	l2 = _strlen(src);
+	lt = l1 + l2 + 1;
+
+	j = 0;
+
+	while (j < n && *src != '\0')
+	{
+		dest[j] = *src;
+		j++;
+		src++;
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}
+
+/**
+ *  _strlen - gives the length of string.
+ *  @str: target string
+ *
+ *  Return: length
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+
+	return (i);
+}
