8270449f29bf5957f3bd89037511e3c40f093dac
Zelalem <zzewdie@gmail.com>
2022-10-05 22:51:55 +0300

    Cprog_Args_3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiplies two args then print to stdout
+ * @argv: arg_vector
+ * @argc: arg_counter
+ *
+ * Return: On success 1.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	mul = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	else if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+
+	return (1);
+}
