aa865ffb1f64422eb4f5f0c436240592bd2ae775
Zelalem <zzewdie@gmail.com>
2022-10-10 23:34:41 +0300

    Cprog_CPP_3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H_
+#define FUNCTION_LIKE_MACRO_H_
+
+/*
+ * macro defining absolute value
+ */
+
+#define ABS(x) ((-x) ? (x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H_ */
