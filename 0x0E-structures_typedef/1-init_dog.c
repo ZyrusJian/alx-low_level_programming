a736f8ccb872d8980687807205fc724c9a34ffca
Zelalem <zzewdie@gmail.com>
2022-10-11 00:31:25 +0300

    Cprog_struct_1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * init_dog - initialize struct
+ * @d: struct type
+ * @name: name entry
+ * @age: age entry
+ * @owner: owner entry
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = *name;
+	(*d).age = age;
+	(*d).owner = *owner;
+}
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -6,5 +6,6 @@
  */
 
 int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* _MAIN_H_ */
