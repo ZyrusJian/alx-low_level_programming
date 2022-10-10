20eb8c7f01080e4019300baf203a4e9cd53e57ee
Zelalem <zzewdie@gmail.com>
2022-10-11 00:39:34 +0300

    Cprog_struct_1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * init_dog - initialize struct
@@ -11,7 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = *name;
+	(*d).name = name;
 	(*d).age = age;
-	(*d).owner = *owner;
+	(*d).owner = owner;
 }
