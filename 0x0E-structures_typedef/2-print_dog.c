547ca979950e4b51d289fa1cbe66fb0afa1c975e
Zelalem <zzewdie@gmail.com>
2022-10-11 23:02:24 +0300

    Cprog_struct_2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,15 +14,15 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)");
 	}
-	else if(isnan((*d).age))
+	else if (isnan((*d).age))
 	{
 		printf("Age: (nil)");
 	}
-	else if((*d).owner == NULL)
+	else if ((*d).owner == NULL)
 	{
 		printf("Owner: (nil)");
 	}
-	else if ( d != NULL)
+	else if (d != NULL)
 	{
 		printf("Name: %s\n", (*d).name);
 		printf("Age: %f\n", (*d).age);
