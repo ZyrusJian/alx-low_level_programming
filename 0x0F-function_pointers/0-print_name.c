8ef5b43509f18e17e8a35ef02c2996c46cef76b1
Zelalem <zzewdie@gmail.com>
2022-10-12 22:22:52 +0300

    Cprog_FP_0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - writes the name to stdout
+ * @name: target
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(char *name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+/*
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_H_ */
