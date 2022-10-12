bc3392710d0567f297da8193717dd415f1358575
Zelalem <zzewdie@gmail.com>
2022-10-13 00:25:56 +0300

    Cprog_FP_3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "calc.h"
+
+
+/**
+ * main - check the code
+ * @argc: arg counter
+ * @argv: arg array
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("\n");
+	return (0);
+}
