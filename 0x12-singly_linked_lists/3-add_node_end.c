609ffed65b3a3f94d6ce0f405501e6e95ac1ae39
Zelalem <zzewdie@gmail.com>
2022-10-21 11:01:10 +0300

    Cprog_lists_3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a node to the list
+ * @head: pointer to the existing list
+ * @str: new list element component
+ *
+ * Return: first node pointer
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *set;
+
+	set = (*head);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = NULL;
+
+	while (set != NULL)
+	{
+		set = set->next
+	}
+	set->next = new;
+
+	return (*head);
+}
