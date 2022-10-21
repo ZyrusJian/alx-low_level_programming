c0261fda8293b4a95f881e4c41bfdf8ac812ecb6
Zelalem <zzewdie@gmail.com>
2022-10-21 06:03:42 +0300

    Cprog_lists_0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,20 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *set;
+	size_t k;
 
 	set = NULL;
 	set = h;
+	k = 0;
 
 	if (h->str == NULL)
 		printf("[0] (nil)");
 
 	while (set != NULL)
 	{
+		k++;
 		printf("[%d] %s\n", set->len, set->str);
 		set = set->next;
 	}
-	return (sizeof(list_t));
+	return (k);
 }
