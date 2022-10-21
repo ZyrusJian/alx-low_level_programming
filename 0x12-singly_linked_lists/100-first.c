393653a58bdd30f47d4a4177f562aa898fffacb2
Zelalem <zzewdie@gmail.com>
2022-10-22 01:44:04 +0300

    Cprog_lists_100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void _first(void) __attribute__ ((constructor));
+
 /**
  * _first - prints before main
  * 
  *
  */
 
-void attribute ((constructor)) _first(void)
+void _first(void) __attribute__ ((constructor))
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
