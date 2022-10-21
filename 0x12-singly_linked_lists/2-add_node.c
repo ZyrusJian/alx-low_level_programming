159ce2e0fbc43ebe6ca8b5487bbe0e5ef698550b
Zelalem <zzewdie@gmail.com>
2022-10-21 10:10:13 +0300

    Cprog_lists_2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the list
+ * @head: pointer to the existing list
+ *
+ * Return: first node pointer
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = (*head);
+	*head = new;
+
+	return (*head);
+}
