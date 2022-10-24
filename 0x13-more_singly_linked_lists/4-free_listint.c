08985edc55e33595d167f8ea848a582ba99dd76e
Zelalem <zzewdie@gmail.com>
2022-10-25 00:53:16 +0300

    Cprog_listsmore_4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the list
+ * @head: pointer to the existing list
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *set;
+
+	set = NULL;
+	if (head == NULL)
+		exit(0);
+
+	set = head;
+
+	while (set != NULL)
+	{
+		set = set->next;
+		free(head);
+		head = set;
+	}
+	head = NULL;
+	if (head == NULL)
+		exit(0);
+}
