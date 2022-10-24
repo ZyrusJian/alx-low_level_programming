0e4140e0988dd98d89b5574e5dca42ba52d04cd5
Zelalem <zzewdie@gmail.com>
2022-10-25 01:50:24 +0300

    Cprog_listsmore_6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - frees the list
+ * @head: pointer to the existing list
+ *
+ * Return: list element n
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *set;
+	int dt;
+
+	if (head == NULL)
+		return (0);
+
+	else
+	{
+		dt = (*head)->n;
+		set = (*head);
+		(*head) = (*head)->next;
+		free(set);
+	}
+	return (dt);
+}
