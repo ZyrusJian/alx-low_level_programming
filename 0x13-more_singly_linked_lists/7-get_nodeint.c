d4639cff46ee12dca9268e4346c40e5096a59628
Zelalem <zzewdie@gmail.com>
2022-10-25 23:52:20 +0300

    Cprog_listsmore_7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - reterives nth node
+ * @head: pointer to the existing list
+ * @index: nth node
+ *
+ * Return: pointer to nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *set;
+	unsigned int k;
+
+	k = 0;
+	if (head == NULL)
+		return (NULL);
+
+	set = head;
+	while ((k != index) && (set != NULL))
+	{
+		set = set->next;
+		k++;
+	}
+	if (k == index)
+		return (set);
+	else if (set == NULL)
+		return (NULL);
+}
