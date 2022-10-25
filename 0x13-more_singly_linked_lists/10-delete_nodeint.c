41ec3c4ddb2d29931562d6efdc677f92108702a6
Zelalem <zzewdie@gmail.com>
2022-10-26 01:11:17 +0300

    Cprog_listsmore_10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			prv = prv->next;
 		rmv = set;
 		set = set->next;
-		prv->next =set;
+		prv->next = set;
 		rmv->next = NULL;
 		free(rmv);
 		return (1);
