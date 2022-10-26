2890b50bdb28d7a8ce24ec06b225cc586c6e2a47
Zelalem <zzewdie@gmail.com>
2022-10-27 00:18:18 +0300

    Cprog_listsmore_10fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *set, *new, *prv;
-	unsigned int k;
+	unsigned int k, i;
 
 	k = 0;
 	if ((*head) == NULL)
