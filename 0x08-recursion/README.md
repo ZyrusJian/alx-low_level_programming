6df81b0cdf9fbbea4796616eb27061113378bc0f
Zelalem <zzewdie@gmail.com>
2022-10-26 11:29:06 +0300

    Cprog_recur_0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+This directory contains Cprograming projects concerning
+with the concept of recurisive (self_reterative) programing.
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* _MAIN_H_ */
