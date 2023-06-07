4c1069ebbec97e41132aa1e349618b6821867c98
Zelalem <zzewdie@gmail.com>
2023-06-07 10:14:29 +0300

    0x08_recursion_v1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+int _powr(int x, int y);
+int _guess(int k, int n);
+
+/**
+ * _sqrt_recursion - compute the square root of a number
+ * @n: target number
+ *
+ * Return: result of computation
+ */
+
+int _sqrt_recursion(int n)
+{
+	int ans, x;
+
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (-1);
+	x = 0;
+	x = _guess(0, n);
+	printf("%d\n", x);
+	ans = _powr(x, 2);
+	if (ans == n)
+		return (ans);
+	return (-1);
+}
+
+/**
+ * _powr - compute x raised to power of y
+ * @x: base no.
+ * @y: power to raise
+ *
+ * Return: result of computation
+ */
+
+int _powr(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _powr(x, y - 1));
+}
+
+/**
+ * _guess- guess the square root of number
+ * @k: initial number
+ * @n: number to compute sqare root
+ *
+ * Return: square root guess
+ */
+
+int _guess(int k, int n)
+{
+	k++;
+	if ((_powr(k, 2)) <= n)
+		_guess(k, n);
+	return (k);
+}
