0f770830787e5e1ee39b19291ec2bda4c63fcc78
Zelalem <zzewdie@gmail.com>
2023-06-09 22:17:52 +0300

    0x08_Tadvanced_v1

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_palindrome - compute if a string is a palindrome
+ * @*s: is a string
+ *
+ * Return: 1 if palindrome other wise 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == "\0")
+		return (1);
+	if (*s != "\0")
+		
+	return (x * _pow_recursion(x, y - 1));
+}
