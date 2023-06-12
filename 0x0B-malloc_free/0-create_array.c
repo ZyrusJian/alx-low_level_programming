bd6f9a05a5ab9c38bc08ad0ff5f1485955b76a9b
Zelalem <zzewdie@gmail.com>
2023-06-12 20:31:42 +0300

    0x0B_malloc0_v1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array of unkown size using malloc
+ * @c: target character to initialize array to.
+ * @size: size of the array to be dynamically allocated
+ *
+ * Return: NULL if it fails or a pointer to the array on success.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = (char *) malloc(size * sizeof(char));
+	for (i = 0; i < size; i++;)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Function prototypes
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /* _MAIN_H_ */
