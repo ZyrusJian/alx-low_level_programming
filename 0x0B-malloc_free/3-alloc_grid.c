715ea48a10a4508992d46597542272e0f3b3cde5
Zelalem <zzewdie@gmail.com>
2023-06-15 22:44:26 +0300

    0x0B_malloc3_v2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void _free(int **arr, int height);
+
 /**
  * **alloc_grid - creates a 2d array of unkown size using malloc
  * @width: column of the 2d array.
@@ -14,26 +16,25 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	/* allocate 2d array memory dynamically */
+	/* check malloc's return */
 	if ((width <= 0) || (height <= 0))
 	{
 		return (NULL);
 	}
 
 	arr = (int **)malloc(height * sizeof(int *));
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(width * sizeof(int));
-	}
-	/* check malloc's return */
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < height; i++)
-	{
 		if (arr[i] == NULL)
 		{
+			free(arr[i]);
+			free(arr);
 			return (NULL);
 		}
 	}
@@ -45,5 +46,27 @@ int **alloc_grid(int width, int height)
 			arr[i][j] = 0;
 		}
 	}
+	if (arr == NULL)
+	{
+		_free(arr, height);
+		return (NULL);
+	}
+	_free(arr, height);
 	return (arr);
+}
+/**
+ * _free - free the 2d array memory allocation
+ * @arr: 2d array.
+ * @height: rows of the 2d array.
+ *
+ */
+void _free(int **arr, int height)
+{
+	int i;
+	/* free allocated memory */
+	for (i = 0; i < height; i++)
+	{
+		free(arr[i]);
+	}
+	free(arr);
 }
