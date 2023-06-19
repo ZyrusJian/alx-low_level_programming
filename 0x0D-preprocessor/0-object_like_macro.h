5eaab7d5dc43340e36b1ce91d1a08aa253700836
Zelalem <zzewdie@gmail.com>
2023-06-19 16:23:06 +0300

    0x0D_prepro_all

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H_
+#define OBJECT_LIKE_MACRO_H_
+
+/*
+ * macro defining size
+ */
+
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H_ */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H_
+#define FUNCTION_LIKE_MACRO_H_
+
+/*
+ * macro defining absolute value
+ */
+
+#define ABS(x) ((x < 0) ? (x * -1) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H_ */
