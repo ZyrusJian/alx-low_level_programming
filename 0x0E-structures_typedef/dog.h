8869a6b5272e094c44675700fdfa89b520c37aa8
Zelalem <zzewdie@gmail.com>
2023-06-19 17:59:42 +0300

    0x0E_struct_v3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,14 @@
  * @owner: owner entry
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
