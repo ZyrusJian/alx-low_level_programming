6793eccf468c1f947511001606528321fdc4c0b0
Zelalem <zzewdie@gmail.com>
2023-06-23 23:24:32 +0300

    0x10_varfunc_v6

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,30 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+
+/**
+ * format_count - counts the number of format
+ * @format: format identifier
+ *
+ * Return: count of format number
+ */
+
+int format_count(const char * const format)
+{
+	int count, i;
+	/* Count number of formats*/
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+				format[i] == 'f' || format[i] == 's')
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
 /**
  * print_all - prints args identified by format
  * @format: format identifier
@@ -8,45 +33,42 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	int len, count, num, i;
+	int fcount, num, argi;
 	char c, *str;
 	float ft;
 
-	len = strlen(format);
-	if (format != NULL)
+	va_start(list, format);
+	fcount = format_count(format);
+	argi = 0;
+	while (argi < fcount)
 	{
-		count = 0;
-		i = 0;
-		while (i < len)
+		switch (format[argi])
 		{
-			va_start(list, format);
-			switch (format[i])
-			{
-				case 'c':
-					c = (char) va_arg(list, int);
-					printf("%c", c);
+			case 'c':
+				c = (char) va_arg(list, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				num = va_arg(list, int);
+				printf("%d", num);
+				break;
+			case 'f':
+				ft = (float) va_arg(list, double);
+				printf("%f", ft);
+				break;
+			case 's':
+				str = va_arg(list, char*);
+				if (str == NULL)
+				{
+					printf("(nil)");
 					break;
-				case 'i':
-					num = va_arg(list, int);
-					printf("%d", num);
-					break;
-				case 'f':
-					ft = (float) va_arg(list, double);
-					printf("%f", ft);
-					break;
-				case 's':
-					str = va_arg(list, char*);
-					printf("%s", str);
-					break;
-				default:
-					break;
-			}
-			va_end(list);
-			if (i != (len - 1))
-				printf(", ");
-			i++;
-			count++;
+				}
+				printf("%s", str);
+				break;
 		}
-		printf("\nNumber of cases: %d\n", count);
+		(argii != (fcount - 1)) && (printf(", "), 1);
+		argi++;
 	}
+	va_end(list);
+	printf("\n");
 }
