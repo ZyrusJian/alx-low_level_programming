57462ae5de7fade2eff613a7e3327bd179d3b51a
Zelalem <zzewdie@gmail.com>
2023-06-30 22:51:06 +0300

    0x12_slinkedlist_v3

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+section .data
+    format db 'Hello, Holberton', 0
+    nl db 10, 0
+
+section .text
+    extern printf
+
+global main
+main:
+    ; Prepare arguments for printf
+    mov rdi, format
+    xor rax, rax   ; Clear RAX register (first parameter for printf)
+    
+    ; Call printf
+    call printf
+    
+    ; Print new line
+    mov rdi, nl
+    xor rax, rax
+    call printf
+    
+    ; Exit program
+    mov eax, 60
+    xor edi, edi
+    syscall
