23dd76d7cb4028a54115e451973e7734e28319dd
Zelalem <zzewdie@gmail.com>
2023-07-26 04:34:43 +0300

    0x14_bitwise_v6

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,14 @@
-#include <main.h>
-
+#include "main.h"
 
 /**
  * print_binary - Prints the binary representation of a number
- * @n: The number to be printed
+ * @n: The number to print
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
-
-	while (mask > 0)
-	{
-		if (n & mask)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		mask >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
+
